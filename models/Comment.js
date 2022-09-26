db9d97c4ba77cc05a58185647939dc783f0d3350
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-09-26 18:13:31 -0500

    created Comment model

---
+++ models/Comment.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        commentId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        commentString: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        blogId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'blog',
+                key: 'blog_id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
+);
+
+module.exports = Comment;
