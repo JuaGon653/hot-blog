144e5f2d5af091e8be759e4b3727ce45d6ebb5d4
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-09-27 10:35:48 -0500

    inserted app.engine

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+const hbs = exphbs.create({});
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -24,6 +25,7 @@ const sess = {
 
 app.use(session(sess));
 
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
