7ae7ca7e045c1b3e48f2f8435e796119d6b0195a
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-09-27 09:44:11 -0500

    created the server.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,9 +2,13 @@ const router = require('express').Router();
 const { Blog } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
     try {
-        
+        const blogData = await Blog.findAll();
+
+        const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+        res.render('homepage', blogs);
     } catch (err) {
         res.status(500).json(err);
     }
