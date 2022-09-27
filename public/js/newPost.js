88756d42b2cf643a50ab3a17dcd4068d600f8931
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-09-27 17:28:14 -0500

    added event listener for create button

--- public/js/newPost.js
+++ public/js/newPost.js
@@ -1,7 +1,31 @@
+const { post } = require("../../controllers/home-routes");
+
 const newPost = async (event) => {
     event.preventDefault();
 
     document.querySelector('#post-form').style['visibility'] = 'visible';
 };
+const createPost = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('#blog-title').value.trim();
+    const content = document.querySelector('#blog-content').value.trim();
+
+    if (title && content) {
+        const response = await fetch('/create-post', {
+            method: 'POST',
+            body: JSON.stringify({ title, content }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to create a new blog.');
+        }
+    }
+
+}
 
 document.querySelector('#new-post').addEventListener('click', newPost);
+document.querySelector('#create').addEventListener('submit', createPost);
