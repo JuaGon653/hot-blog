47fcf814003d59c906896f45a0274928dead4035
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-09-27 09:18:36 -0500

    seeds users

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,15 +1,10 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
-
-const userData = require('./userData.json');
+const seedUser = require('./userData');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
 
-    await User.bulkCreate(userData, {
-        individualHooks: true,
-        returning: true
-    });
+    await seedUser();
 
     process.exit(0);
 };
