3556442148c2c988ca7db2c85e26063998d604b6
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-09-27 09:25:32 -0500

    created blog seed

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,10 +1,13 @@
 const sequelize = require('../config/connection');
 const seedUser = require('./userData');
+const seedBlogs = require('./blogData');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
 
     await seedUser();
+
+    await seedBlogs();
 
     process.exit(0);
 };
