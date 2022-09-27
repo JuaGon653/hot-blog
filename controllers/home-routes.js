db4c6dca60c5939dedd13b839ad2db4b5ce862ed
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-09-27 14:08:55 -0500

    able to log out/in with ease

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -17,7 +17,7 @@ router.get('/', withAuth, async (req, res) => {
 
         const blogs = blogData.map((blog) => blog.get({ plain: true }));
         console.log(blogs);
-        res.render('homepage', { blogs });
+        res.render('homepage', { blogs, logged_in: req.session.logged_in });
     } catch (err) {
         res.status(500).json(err);
     }
