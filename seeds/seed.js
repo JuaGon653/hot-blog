dc42dcecacb3c4053332b42954fe1431aaa730d0
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-09-29 21:40:37 -0500

    seeds comments

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,6 +1,7 @@
 const sequelize = require('../config/connection');
 const seedUser = require('./userData');
 const seedBlogs = require('./blogData');
+const seedComments = require('./commentData');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
@@ -8,6 +9,8 @@ const seedDatabase = async () => {
     await seedUser();
 
     await seedBlogs();
+
+    await seedComments();
 
     process.exit(0);
 };
