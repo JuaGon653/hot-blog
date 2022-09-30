e5f239b21d07eaebf693439b91670901aa8c9b5c
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-09-30 11:56:28 -0500

    user_id is now in the seeds

--- seeds/commentData.js
+++ seeds/commentData.js
@@ -3,11 +3,13 @@ const { Comment } = require('../models');
 const commentData = [
     {
         content: "This is a comment",
-        blog_id: 1
+        blog_id: 1,
+        user_id: 2
     },
     {
         content: "Another comment",
-        blog_id: 2
+        blog_id: 2,
+        user_id: 1
     }
 ];
 
