f0fbf627755219fd8d777f1775ad895aa60ca442
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-10-01 12:09:55 -0500

    adjusted helper function name

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,7 +2,7 @@ module.exports = {
     format_date: (date) => {
         return `${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`;
     },
-    format_link: (blogId) => {
+    edit_blog_link: (blogId) => {
         return `/edit-blog/${blogId}`;
     },
     view_post_link: (blogId) => {
